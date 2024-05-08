ff8e88de33fcceb421b384201c7c71dbfe99b9c5
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-05-08 13:47:35 +0300

    mod Title

--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -6,10 +6,9 @@ export const ContainerStyled = styled.div`
   //   max-width: 375px;
   margin: 0 auto;
   width: 100%;
-  padding-top: 40px;
-  background-color: #FFD4D0;
-  font-family: "Montserrat", sans-serif;
-
+  padding: 40px 0;
+  background-color: #ffd4d0;
+  font-family: 'Montserrat', sans-serif;
 
   //   @media screen and (min-width: 768px) {
   //     max-width: 768px;
@@ -25,23 +24,29 @@ export const ContainerStyled = styled.div`
   //   }
 `;
 
-export const TitleContainer = styled.div``;
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export const MainTitle = styled.h1`
-font-size: 48px;
-font-weight: 700;
-line-height: 1.5;
-color: #FC3314;
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 1.5;
+  color: #fc3314;
+  margin-top: 18px;
+  margin-bottom: 8px;
 `;
 export const ProfessionTitle = styled.h2`
-font-size: 20px;
-font-weight: 400;
-line-height: 1.5;
-color: #1F1F1F;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #1f1f1f;
 `;
 export const AboutText = styled.p`
-font-size: 16px;
-font-weight: 400;
-line-height: 1.5;
-color: #1F1F1F;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #1f1f1f;
 `;
