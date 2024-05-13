e7999390181d81e1cc36d669c0ba15278dfe1cc7
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-05-13 16:15:29 +0300

    del Subject

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Subject from './Pages/Subject';
 import NotFound from './NotFound/NotFound';
 import Layout from './Layout/Layout';
 
@@ -9,7 +8,6 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout><Home /></Layout>} />
-      <Route path="/subject" element={<Layout><Subject /></Layout>} />
       <Route path="/about" element={<Layout><About /></Layout>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
