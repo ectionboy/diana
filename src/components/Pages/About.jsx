bd6aa780fc9571fb9ec0cb9dc23625f06debd3a9
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-05-20 15:42:07 +0300

    upd counter

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,58 +1,46 @@
 import React, { useEffect, useState } from 'react';
-    const startDate = new Date('2022-11-21');
 
 const About = () => {
-  const [timeElapsed, setTimeElapsed] = useState({
+  const [time, setTime] = useState({
+    months: 0,
     days: 0,
     hours: 0,
     minutes: 0,
-    seconds: 0
+    seconds: 0,
   });
 
   useEffect(() => {
+    const startDate = new Date('2022-11-21T00:00:00Z'); 
+    const calculateTimeDifference = () => {
+      const now = new Date();
+      const diff = now - startDate;
 
-    const updateTime = () => {
-      const currentDate = new Date();
-      const differenceInTime = currentDate - startDate;
+      const months = now.getMonth() - startDate.getMonth() +
+        (12 * (now.getFullYear() - startDate.getFullYear()));
 
-      const differenceInDays = Math.floor(differenceInTime / (1000 * 60 * 60 * 24));
-      const remainingTimeAfterDays = differenceInTime % (1000 * 60 * 60 * 24);
-      const differenceInHours = Math.floor(remainingTimeAfterDays / (1000 * 60 * 60));
-      const remainingTimeAfterHours = remainingTimeAfterDays % (1000 * 60 * 60);
-      const differenceInMinutes = Math.floor(remainingTimeAfterHours / (1000 * 60));
-      const remainingTimeAfterMinutes = remainingTimeAfterHours % (1000 * 60);
-      const differenceInSeconds = Math.floor(remainingTimeAfterMinutes / 1000);
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / 1000 / 60) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
 
-
-
-      setTimeElapsed({
-        days: differenceInDays,
-        hours: differenceInHours,
-        minutes: differenceInMinutes,
-        seconds: differenceInSeconds
-      });
+      setTime({ months, days, hours, minutes, seconds });
     };
 
-    const intervalId = setInterval(updateTime, 1000);
+    const intervalId = setInterval(calculateTimeDifference, 1000);
 
-    // Викликаємо updateTime одразу, щоб відображати час при завантаженні сторінки
-    updateTime();
-
-    return () => {
-      clearInterval(intervalId);
-    };
+    return () => clearInterval(intervalId);
   }, []);
-  console.log(timeElapsed.seconds)
 
   return (
     <>
     <br />
       <h2>Ми разом:</h2>
       <br />
-      <p>Днів: {timeElapsed.days}</p>
-      <p>Годин: {timeElapsed.hours}</p>
-      <p>Хвилин: {timeElapsed.minutes}</p>
-      <p>Секунд: {timeElapsed.seconds}</p>
+      <p>Місяців: {time.months}</p>
+      <p>Днів: {time.days}</p>
+      <p>Годин: {time.hours}</p>
+      <p>Хвилин: {time.minutes}</p>
+      <p>Секунд: {time.seconds}</p>
       <br />
     </>
   );
